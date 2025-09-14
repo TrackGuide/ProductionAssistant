adbb8cb300e38c5daab681e07a7aaf5719df3b9c
TrackGuide <dustinspaceproductions@gmail.com>
2025-09-14 13:21:16 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -39,6 +39,8 @@ import { LibraryModal } from './src/components/LibraryModal.tsx';
 import { MarkdownRenderer } from './src/components/MarkdownRenderer.tsx';
 import ToplineBuilderPanel from './src/components/ToplineBuilderPanel.tsx';
 import { stopPlayback } from './src/services/audioService.ts';
+import { generateToplineMidi } from './src/services/toplineMidi';
+
 import { APP_TITLE, LOCAL_STORAGE_KEY, GENRE_SUGGESTIONS, VIBE_SUGGESTIONS, DAW_SUGGESTIONS, MIDI_DEFAULT_SETTINGS, MIDI_SCALES, MIDI_CHORD_PROGRESSIONS, MIDI_TEMPO_RANGES, LAST_USED_DAW_KEY, LAST_USED_PLUGINS_KEY } from './src/constants/constants';
 
 // Simple logo
