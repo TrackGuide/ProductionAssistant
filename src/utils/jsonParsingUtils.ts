9155bd26d013d2704e9356e52e91bb0641726bd1
TrackGuide <dustinspaceproductions@gmail.com>
2025-09-15 01:26:47 -0400

    Update jsonParsingUtils.ts

--- src/utils/jsonParsingUtils.ts
+++ src/utils/jsonParsingUtils.ts
@@ -391,7 +391,6 @@ export function getMinimalMidiPattern(): GeneratedMidiPatterns {
         pitch: "C5",
       },
     ],
-    topline_melody: [],
     drums: {
       kick: [{ time: 0, duration: 0.25, velocity: 120 }],
       snare: [{ time: 2, duration: 0.25, velocity: 100 }],
